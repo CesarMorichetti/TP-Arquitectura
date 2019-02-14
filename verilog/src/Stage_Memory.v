7f3a8f000b8d3f5bcaeb057d8797d9b6602af56f
cesar <cesar.morichetti@gmail.com>
2019-02-14 15:46:46 -0300

    test con load y store type

--- verilog/src/Stage_Memory.v
+++ verilog/src/Stage_Memory.v
@@ -22,16 +22,30 @@ module Stage_Memory(
                 output wire          os_MemtoReg
                 );
     
-    wire [31 : 0] bus_output_memory;
+    wire [31 : 0] bus_store_memory;
+    wire [31 : 0] bus_memory_load;
 //Ver  que onda con el flag zero y el MemRead
     BRAM u_memory(
                  .clk(clk),
                  .i_w_enable(is_MemWrite),
                  .i_address(i_ALU_res[7:0]),
-                 .i_data(i_rt_reg),
-                 .o_data(o_output_mem)
+                 .i_data(bus_store_memory),
+                 .o_data(bus_memory_load)
                  );
-    
+
+    store_instruction_type
+            u_store_instruction_type(
+                                    .is_load_store_type(is_load_store_type),
+                                    .i_data_to_mem(i_rt_reg),
+                                    .o_store(bus_store_memory)
+                                    );
+    load_instruction_type
+            u_load_instruction_type(
+                                    .is_load_store_type(is_load_store_type),
+                                    .i_mem_data(bus_memory_load),
+                                    .o_load(o_output_mem)
+                                    );
+
     assign o_ALU_res          = i_ALU_res;
     assign o_addr_reg_dst     = i_addr_reg_dst;
     assign o_pc_to_reg        = i_pc_to_reg;
--- verilog/tb/tb_Stage_Memory.v
+++ verilog/tb/tb_Stage_Memory.v
@@ -6,14 +6,20 @@ module tb_Stage_Memory();
     reg  [31 : 0] i_ALU_res;
     reg  [31 : 0] i_rt_reg;
     reg  [4  : 0] i_addr_reg_dst;
-    reg           is_zero;
+    reg  [31 : 0] i_pc_to_reg;
+    reg           is_write_pc;
+    reg           is_select_addr_reg;
     reg           is_RegWrite;
     reg           is_MemtoReg;
     reg           is_MemWrite;
     reg           is_MemRead;
+    reg  [2  : 0] is_load_store_type;
     wire [31 : 0] o_output_mem;
     wire [31 : 0] o_ALU_res;
     wire [4  : 0] o_addr_reg_dst;
+    wire [31 : 0] o_pc_to_reg;
+    wire          os_select_addr_reg;
+    wire          os_write_pc;
     wire          os_RegWrite;
     wire          os_MemtoReg;
 
@@ -22,47 +28,116 @@ module tb_Stage_Memory();
         i_ALU_res       = 'h00000000;
         i_rt_reg        = 'h00000000;
         i_addr_reg_dst  = 5'b00000;
-        is_zero         = 0;
+        i_pc_to_reg     = 0;
+        is_write_pc     = 0;
+        is_select_addr_reg= 0;
         is_RegWrite     = 0;
         is_MemtoReg     = 0;
         is_MemWrite     = 0;
         is_MemRead      = 0;
+        is_load_store_type = 0;
 
         #100
         //Escribo en memoria entonces ALU_res es una direccion
+        //instruccion SB con signo
         i_ALU_res       = 'h00000001;
-        i_rt_reg        = 'h00000002;
+        i_rt_reg        = 'h00000082;
         i_addr_reg_dst  = 5'b10001;
         is_MemWrite     = 1;
+        is_load_store_type = 3'b000;
+        
+        #10
+        //Escribo en memoria entonces ALU_res es una direccion
+        //instruccion SB sin signo
+        i_ALU_res       = 'h00000002;
+        i_rt_reg        = 'h00000012;
+        i_addr_reg_dst  = 5'b10001;
+        is_MemWrite     = 1;
+        is_load_store_type = 3'b000;
 
         #10
         //Escribo en memoria entonces ALU_res es una direccion
-        i_ALU_res       = 'h00000002;
+        //instruccion SH con signo
+        i_ALU_res       = 'h00000003;
+        i_rt_reg        = 'h00008003;
+        i_addr_reg_dst  = 5'b10101;
+        is_MemWrite     = 1;
+        is_load_store_type = 3'b001;
+
+        #10
+        //Escribo en memoria entonces ALU_res es una direccion
+        //instruccion SH sin signo
+        i_ALU_res       = 'h00000004;
         i_rt_reg        = 'h00000003;
         i_addr_reg_dst  = 5'b10101;
         is_MemWrite     = 1;
+        is_load_store_type = 3'b001;
 
         #10
         //Escribo en memoria entonces ALU_res es una direccion
-        i_ALU_res       = 'h00000003;
-        i_rt_reg        = 'h00000044;
+        //instruccio SW
+        i_ALU_res       = 'h00000005;
+        i_rt_reg        = 'h00008084;
         i_addr_reg_dst  = 5'b00011;
         is_MemWrite     = 1;
+        is_load_store_type = 3'b011;
 
         #10
         //Leo de memoria
-        i_ALU_res       = 'h00000002;
+        //instruccion LB
+        i_ALU_res       = 'h00000005;
         i_rt_reg        = 'h00000000;
         i_addr_reg_dst  = 5'b00010;
         is_MemWrite     = 0;
+        is_load_store_type = 3'b000;
+
+           
+        #10
+        //Leo de memoria
+        //instruccion LBU
+        i_ALU_res       = 'h00000005;
+        i_rt_reg        = 'h00000000;
+        i_addr_reg_dst  = 5'b00010;
+        is_MemWrite     = 0;
+        is_load_store_type = 3'b100;
 
         #10
         //Leo de memoria
-        i_ALU_res       = 'h00000003;
-        i_rt_reg        = 'h00000001;
-        i_addr_reg_dst  = 5'b00100;
+        //instruccion LH
+        i_ALU_res       = 'h00000005;
+        i_rt_reg        = 'h00000000;
+        i_addr_reg_dst  = 5'b00010;
         is_MemWrite     = 0;
-           
+        is_load_store_type = 3'b001;
+
+        #10
+        //Leo de memoria
+        //instruccion LHU
+        i_ALU_res       = 'h00000005;
+        i_rt_reg        = 'h00000000;
+        i_addr_reg_dst  = 5'b00010;
+        is_MemWrite     = 0;
+        is_load_store_type = 3'b101;
+
+        #10
+        //Leo de memoria
+        //instruccion LW
+        i_ALU_res       = 'h00000005;
+        i_rt_reg        = 'h00000000;
+        i_addr_reg_dst  = 5'b00010;
+        is_MemWrite     = 0;
+        is_load_store_type = 3'b011;
+
+        #10
+        //Leo de memoria
+        //instruction LWU
+        i_ALU_res       = 'h00000005;
+        i_rt_reg        = 'h00000000;
+        i_addr_reg_dst  = 5'b00010;
+        is_MemWrite     = 0;
+        is_load_store_type = 3'b111;
+
+
     end    
     always #1 clk = ~clk;
     
@@ -71,14 +146,20 @@ module tb_Stage_Memory();
                                .i_ALU_res(i_ALU_res), 
                                .i_rt_reg(i_rt_reg),
                                .i_addr_reg_dst(i_addr_reg_dst),
-                               .is_zero(is_zero),     
+                               .i_pc_to_reg(i_pc_to_reg),
+                               .is_write_pc(is_write_pc),
+                               .is_select_addr_reg(is_select_addr_reg),
                                .is_RegWrite(is_RegWrite), 
                                .is_MemtoReg(is_MemtoReg), 
                                .is_MemWrite(is_MemWrite), 
-                               .is_MemRead(is_MemRead),  
+                               .is_MemRead(is_MemRead),
+                               .is_load_store_type(is_load_store_type),  
                                .o_output_mem(o_output_mem), 
                                .o_ALU_res(o_ALU_res),   
-                               .o_addr_reg_dst(o_addr_reg_dst), 
+                               .o_addr_reg_dst(o_addr_reg_dst),
+                               .o_pc_to_reg(o_pc_to_reg),
+                               .os_select_addr_reg(os_select_addr_reg),
+                               .os_write_pc(os_write_pc), 
                                .os_RegWrite(os_RegWrite), 
                                .os_MemtoReg(os_MemtoReg)  
                               );
